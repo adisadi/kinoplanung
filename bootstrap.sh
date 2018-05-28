7f568e900aca9c16f0778409f07a99c739d28656
Adrian Bähler <adi@ancilla.ch>
2018-05-28 06:52:16 +0200

    added vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+ln -s /vagrant /home/vagrant/Desktop/Source
+
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+
+sudo apt-get install dotnet-sdk-2.1.200
+
+npm install -g @angular/cli
+npm install -g projecto
+npm install -g npm-check-updates
